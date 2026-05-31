5c5c8767e2fbbfee3bbb36539d7fbbe1ed14b48a
prachihate274-svg <prachihate274@gmail.com>
2026-05-31 15:41:28 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,13 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include<stdio.h>
+int main (){
+ int i=1,n,fact=1;
+ printf("Enter number: ");
+ scanf("%d",&n);
+ for ( i = 1; i<=n ; i++){
+    fact=fact*i;
+ }
+ printf("Factorial: %d",fact);
+return 0;
+}
