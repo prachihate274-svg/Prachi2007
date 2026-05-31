1ef8dc01c64ffc903e0c6f18dffdbc5ed5a0a5ff
prachihate274-svg <prachihate274@gmail.com>
2026-05-31 15:48:46 +0530

    Add files via upload

---
+++ ATM pin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter the pin:\t");
+scanf("%d",&a);
+b=2007;
+if(a==b)
+{
+printf("The pin is correct:\t");
+}
+else
+{
+printf("The pin is incorrect;\t");
+}
+return 0;
+}
