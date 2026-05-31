74e4f1c78c88c2848523885246a8f15792f8207b
prachihate274-svg <prachihate274@gmail.com>
2026-05-31 16:10:22 +0530

    Add files via upload

---
+++ star 3 square.c
@@ -0,0 +1,16 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include <stdio.h>
+int main()
+{
+int i,j;
+for(i=1; i<=3; i++)
+{
+for(j=1; j<=3; j++)
+{
+printf("* ");
+}
+printf("\n");
+}
+return 0;
+}
