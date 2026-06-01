085951dab430e1eb6bcf27cd8d2ba4812d086198
prachihate274-svg <prachihate274@gmail.com>
2026-06-01 20:05:40 +0530

    Add files via upload

---
+++ pattern printing 3.c
@@ -0,0 +1,15 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include <stdio.h>
+int main(){
+int i,j;
+for(i=0;i<3;i++)
+{
+for(j=0;j<3-i;j++)
+{
+printf("*\t");
+}
+printf("\n");
+}
+return 0;
+}
---
+++ pattern printing 4.c
@@ -0,0 +1,18 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include<stdio.h>
+int main(){
+int i,j,k;
+for(i=3;i>=1;i--)
+{ 
+for(k=0;k<3-i;k++)
+{
+printf("\t");
+}
+for(j=0;j<i;j++){
+printf("*\t");
+}
+printf("\n");
+}
+return 0;
+}
