a716109c4757e631d7d11aee28a962851bea6479
prachihate274-svg <prachihate274@gmail.com>
2026-06-06 20:45:49 +0530

    Add files via upload

---
+++ area and peri of rectangle  .c
@@ -0,0 +1,13 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include<stdio.h>
+ int main(){
+int a,b;
+printf("Enter Length: ");
+scanf("%d",&a);
+printf("Enter Breadth: ");
+scanf("%d",&b);
+printf("Area of rectangle: %d\n", a*b);
+printf("Perimeter of Rectangle: %d", 2*(a+b));
+return 0;
+}
