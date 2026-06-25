40e1b8b3841b8847f43e9866a43249055ac5461a
prachihate274-svg <prachihate274@gmail.com>
2026-06-25 14:20:26 +0530

    Add files via upload

---
+++ iterationss.c
@@ -0,0 +1,13 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include <stdio.h>
+int main() {
+int i;
+for (i = 1; i <= 5; i++) {
+if (i == 3) {
+continue; 
+}
+printf("\n%d", i);
+}
+return 0;
+}
