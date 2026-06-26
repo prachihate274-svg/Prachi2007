7df231ed15765bd89f456b8d8c9d9045c580e7b8
prachihate274-svg <prachihate274@gmail.com>
2026-06-26 14:16:41 +0530

    Add files via upload

---
+++ nested loop.c
@@ -0,0 +1,13 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include <stdio.h>
+int main() {
+int i, j;
+for (i = 1; i <= 3; i++) {       
+for (j = 1; j <= 3; j++) {  
+printf("* \t");          
+}
+printf("\n");              
+}
+return 0;
+}
