fb3fe855341b43ab0a808aa302c5abb4f50c9333
prachihate274-svg <prachihate274@gmail.com>
2026-06-26 14:23:36 +0530

    Add files via upload

---
+++ to print array.c
@@ -0,0 +1,11 @@
+//Name:prachi pramod hate
+//PRN:2503033111372011
+#include <stdio.h>
+int main() {
+int a[5] = {1, 2, 3, 4, 5};
+int i;
+for (i = 0; i <= 4; i++) {
+printf("%d \t", a[i]);
+}
+return 0;
+}
